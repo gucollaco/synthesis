a8ac84b4a55c43a30732090b5ec0ac2ce0abb80d
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2019-12-20 00:52:27 -0300

    Navbar linking to pages; templating structure modified

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["CACHE_TYPE"] = "null"
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home/home.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio/portfolio.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume/resume.html')
+
+@app.route('/education')
+def education():
+    return render_template('education/education.html')
 
 @app.route('/about')
 def about():
-    return render_template('index.html')
-
-@app.route('/courses')
-def courses():
-    return render_template('index.html')
+    return render_template('about/about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
