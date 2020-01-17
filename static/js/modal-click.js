64384f98a78262f6d1daae141ebef5e981ab5342
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-01-16 21:51:11 -0300

    some fixes; text being adjustes on mobile (about page)

--- static/js/modal-click.js
+++ static/js/modal-click.js
@@ -1,66 +1,76 @@
-document.querySelector('.show-modal-echo').addEventListener('click', function(event) {
-    const modal = document.getElementById('modal-echo');
-    const html = document.querySelector('html');
+const modalEcho = document.querySelector('.show-modal-echo');
+const modalRango = document.querySelector('.show-modal-rango');
+const modalPac = document.querySelector('.show-modal-pac');
+const modalDraw = document.querySelector('.show-modal-draw');
 
-    modal.classList.add('is-active');
-    html.classList.add('is-clipped');
-});
+if (modalEcho) {
+    document.querySelector('.show-modal-echo').addEventListener('click', function(event) {
+        const modal = document.getElementById('modal-echo');
+        const html = document.querySelector('html');
 
-document.querySelector('.modal-button-close-echo').addEventListener('click', function(event) {
-    const modal = document.getElementById('modal-echo');
-    const html = document.querySelector('html');
+        modal.classList.add('is-active');
+        html.classList.add('is-clipped');
+    });
 
-    modal.classList.remove('is-active');
-    html.classList.remove('is-clipped');
-});
+    document.querySelector('.modal-button-close-echo').addEventListener('click', function(event) {
+        const modal = document.getElementById('modal-echo');
+        const html = document.querySelector('html');
 
+        modal.classList.remove('is-active');
+        html.classList.remove('is-clipped');
+    });
+}
 
-document.querySelector('.show-modal-rango').addEventListener('click', function(event) {
-    const modal = document.getElementById('modal-rango');
-    const html = document.querySelector('html');
+if (modalRango) {
+    document.querySelector('.show-modal-rango').addEventListener('click', function(event) {
+        const modal = document.getElementById('modal-rango');
+        const html = document.querySelector('html');
 
-    modal.classList.add('is-active');
-    html.classList.add('is-clipped');
-});
+        modal.classList.add('is-active');
+        html.classList.add('is-clipped');
+    });
 
-document.querySelector('.modal-button-close-rango').addEventListener('click', function(event) {
-    const modal = document.getElementById('modal-rango');
-    const html = document.querySelector('html');
+    document.querySelector('.modal-button-close-rango').addEventListener('click', function(event) {
+        const modal = document.getElementById('modal-rango');
+        const html = document.querySelector('html');
 
-    modal.classList.remove('is-active');
-    html.classList.remove('is-clipped');
-});
+        modal.classList.remove('is-active');
+        html.classList.remove('is-clipped');
+    });
+}
 
+if (modalPac) {
+    document.querySelector('.show-modal-pac').addEventListener('click', function(event) {
+        const modal = document.getElementById('modal-pac');
+        const html = document.querySelector('html');
 
-document.querySelector('.show-modal-pac').addEventListener('click', function(event) {
-    const modal = document.getElementById('modal-pac');
-    const html = document.querySelector('html');
+        modal.classList.add('is-active');
+        html.classList.add('is-clipped');
+    });
 
-    modal.classList.add('is-active');
-    html.classList.add('is-clipped');
-});
+    document.querySelector('.modal-button-close-pac').addEventListener('click', function(event) {
+        const modal = document.getElementById('modal-pac');
+        const html = document.querySelector('html');
 
-document.querySelector('.modal-button-close-pac').addEventListener('click', function(event) {
-    const modal = document.getElementById('modal-pac');
-    const html = document.querySelector('html');
+        modal.classList.remove('is-active');
+        html.classList.remove('is-clipped');
+    });
+}
 
-    modal.classList.remove('is-active');
-    html.classList.remove('is-clipped');
-});
+if (modalDraw) {
+    document.querySelector('.show-modal-draw').addEventListener('click', function(event) {
+        const modal = document.getElementById('modal-draw');
+        const html = document.querySelector('html');
 
+        modal.classList.add('is-active');
+        html.classList.add('is-clipped');
+    });
 
-document.querySelector('.show-modal-draw').addEventListener('click', function(event) {
-    const modal = document.getElementById('modal-draw');
-    const html = document.querySelector('html');
+    document.querySelector('.modal-button-close-draw').addEventListener('click', function(event) {
+        const modal = document.getElementById('modal-draw');
+        const html = document.querySelector('html');
 
-    modal.classList.add('is-active');
-    html.classList.add('is-clipped');
-});
-
-document.querySelector('.modal-button-close-draw').addEventListener('click', function(event) {
-    const modal = document.getElementById('modal-draw');
-    const html = document.querySelector('html');
-
-    modal.classList.remove('is-active');
-    html.classList.remove('is-clipped');
-});
+        modal.classList.remove('is-active');
+        html.classList.remove('is-clipped');
+    });
+}
