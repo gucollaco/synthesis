ed08aa220d0ff8bea9ecb0965bb172a1e86d66bc
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-01-16 21:25:22 -0300

    modals showing up correctly, for each portfolio card (code will be enhanced later, for now, i just wanna get it done)

--- static/js/modal-click.js
+++ static/js/modal-click.js
@@ -1,13 +1,64 @@
 document.querySelector('.show-modal-echo').addEventListener('click', function(event) {
-    const modal = document.querySelector('.modal');
+    const modal = document.getElementById('modal-echo');
     const html = document.querySelector('html');
 
     modal.classList.add('is-active');
     html.classList.add('is-clipped');
 });
 
-document.querySelector('.modal-button-close').addEventListener('click', function(event) {
-    const modal = document.querySelector('.modal');
+document.querySelector('.modal-button-close-echo').addEventListener('click', function(event) {
+    const modal = document.getElementById('modal-echo');
+    const html = document.querySelector('html');
+
+    modal.classList.remove('is-active');
+    html.classList.remove('is-clipped');
+});
+
+
+document.querySelector('.show-modal-rango').addEventListener('click', function(event) {
+    const modal = document.getElementById('modal-rango');
+    const html = document.querySelector('html');
+
+    modal.classList.add('is-active');
+    html.classList.add('is-clipped');
+});
+
+document.querySelector('.modal-button-close-rango').addEventListener('click', function(event) {
+    const modal = document.getElementById('modal-rango');
+    const html = document.querySelector('html');
+
+    modal.classList.remove('is-active');
+    html.classList.remove('is-clipped');
+});
+
+
+document.querySelector('.show-modal-pac').addEventListener('click', function(event) {
+    const modal = document.getElementById('modal-pac');
+    const html = document.querySelector('html');
+
+    modal.classList.add('is-active');
+    html.classList.add('is-clipped');
+});
+
+document.querySelector('.modal-button-close-pac').addEventListener('click', function(event) {
+    const modal = document.getElementById('modal-pac');
+    const html = document.querySelector('html');
+
+    modal.classList.remove('is-active');
+    html.classList.remove('is-clipped');
+});
+
+
+document.querySelector('.show-modal-draw').addEventListener('click', function(event) {
+    const modal = document.getElementById('modal-draw');
+    const html = document.querySelector('html');
+
+    modal.classList.add('is-active');
+    html.classList.add('is-clipped');
+});
+
+document.querySelector('.modal-button-close-draw').addEventListener('click', function(event) {
+    const modal = document.getElementById('modal-draw');
     const html = document.querySelector('html');
 
     modal.classList.remove('is-active');
