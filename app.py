c39396dc884ec407b9b982ae3a652cc44a2be49b
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-02-02 11:16:50 -0300

    trailing slash added on routes

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ app.config["CACHE_TYPE"] = "null"
 def home():
     return render_template('home/home.html')
 
-@app.route('/portfolio')
+@app.route('/portfolio/')
 def portfolio():
     return render_template('portfolio/portfolio.html')
 
-@app.route('/journey')
+@app.route('/journey/')
 def journey():
     return render_template('journey/journey.html')
 
-@app.route('/about')
+@app.route('/about/')
 def about():
     return render_template('about/about.html')
 
-@app.route('/favicon.ico')
+@app.route('/favicon.ico/')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
