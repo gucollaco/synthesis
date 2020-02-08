30fa6f9e4f7ffcbb05e60f54ed8c9f0ead811bfd
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-02-07 23:25:28 -0300

    modal opening correctly

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 app.config["CACHE_TYPE"] = "null"
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def home():
     return render_template('home/home.html')
 
-@app.route('/portfolio')
+@app.route('/portfolio', strict_slashes=False)
 def portfolio():
     return render_template('portfolio/portfolio.html')
 
-@app.route('/journey')
+@app.route('/journey', strict_slashes=False)
 def journey():
     return render_template('journey/journey.html')
 
-@app.route('/about')
+@app.route('/about', strict_slashes=False)
 def about():
     return render_template('about/about.html')
 
--- static/js/modal-click.js
+++ static/js/modal-click.js
@@ -1,9 +1,16 @@
-const modalEcho = document.querySelector('.show-modal-echo');
-const modalRango = document.querySelector('.show-modal-rango');
-const modalPac = document.querySelector('.show-modal-pac');
-const modalDraw = document.querySelector('.show-modal-draw');
+document.addEventListener("mousemove", function(event) {
+    const modalEcho = document.querySelector('.show-modal-echo');
+    const modalRango = document.querySelector('.show-modal-rango');
+    const modalPac = document.querySelector('.show-modal-pac');
+    const modalDraw = document.querySelector('.show-modal-draw');
+    
+    if(modalEcho) updateModalEcho()
+    if(modalRango) updateModalRango()
+    if(modalPac) updateModalPac()
+    if(modalDraw) updateModalDraw()
+});
 
-if (modalEcho) {
+function updateModalEcho() {
     document.querySelector('.show-modal-echo').addEventListener('click', function(event) {
         const modal = document.getElementById('modal-echo');
         const html = document.querySelector('html');
@@ -21,7 +28,7 @@ if (modalEcho) {
     });
 }
 
-if (modalRango) {
+function updateModalRango() {
     document.querySelector('.show-modal-rango').addEventListener('click', function(event) {
         const modal = document.getElementById('modal-rango');
         const html = document.querySelector('html');
@@ -39,7 +46,7 @@ if (modalRango) {
     });
 }
 
-if (modalPac) {
+function updateModalPac() {
     document.querySelector('.show-modal-pac').addEventListener('click', function(event) {
         const modal = document.getElementById('modal-pac');
         const html = document.querySelector('html');
@@ -57,7 +64,7 @@ if (modalPac) {
     });
 }
 
-if (modalDraw) {
+function updateModalDraw() {
     document.querySelector('.show-modal-draw').addEventListener('click', function(event) {
         const modal = document.getElementById('modal-draw');
         const html = document.querySelector('html');
@@ -73,4 +80,4 @@ if (modalDraw) {
         modal.classList.remove('is-active');
         html.classList.remove('is-clipped');
     });
-}
+}    
